245e8c112fce0164398c66994e0f2cbe75588a51
吉成樹 <tatsukichi@yoshinarijunoMacBook-Air.local>
2020-05-03 01:45:01 +0900

    chapter6 end

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.6.5'
 
 
 gem 'rails', '~> 6.0.2', '>= 6.0.2.2'
+gem 'bcrypt', '3.1.12'
 gem 'sqlite3', '~> 1.4'
 gem 'puma', '~> 4.1'
 gem 'sass-rails', '>= 6'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_145629) do
+ActiveRecord::Schema.define(version: 2020_05_02_061545) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
